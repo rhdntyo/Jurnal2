21a2be2c5c96106942539f969f60e5985f7f9350
rhdntyo <rhdynaristyo@gmail.com>
2024-09-20 15:09:49 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Nama:Rahadiyan Aristyo");
+        System.out.println("NIM:607062400052");
+        System.out.println("Kelas:D3IF4801");
+    }
+}
